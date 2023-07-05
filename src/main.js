e20a8f6fbf744389f91b3695fe5eb4948e963c81
Daniel <rizhendan@gmail.com>
2023-07-05 16:18:48 -0400

    update deploy

--- src/main.js
+++ src/main.js
@@ -16,7 +16,7 @@ import ToastService from 'primevue/toastservice';
 // Prime vue theme, core, icons, and others
 import 'primevue/resources/themes/md-light-indigo/theme.css';
 import 'primevue/resources/primevue.min.css';
-import 'primeicons/primeicons.css';   
+import 'primeicons/primeicons.css';
 import '/node_modules/primeflex/primeflex.css'; // PrimeFlex
 
 /* import the fontawesome core */
